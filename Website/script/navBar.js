941ad4a7d7d831c2b8582605fe88a3776fbc2452
Endre Sørmo Rundsveen <endresr@stud.ntnu.no>
2019-10-03 09:49:39 +0200

    changed menubar from having to change the links for each page. Now they use relative links.

--- Website/script/navBar.js
+++ Website/script/navBar.js
@@ -7,13 +7,14 @@ function toggleClick(){
         aboutvar.style.display='none'
     }
 };
-
+/* Non-necessary code when using relative links from main page
 function currentdirectory(){
     var a = window.location.href;
     var b = a.split('/');
     return b[b.length-2]
 };
-
+*/
+/* Outdated
 function loadNavBar(){
     var y = currentdirectory()
     var nav = document.createElement('DIV');
@@ -22,7 +23,7 @@ function loadNavBar(){
         ['about/contact.html','Contact us','Kontakt oss'],['about/blog.html','Blog','Aktuelt'],
         ['about/calendar.html','Calendar','Kalender'],['img/logo.svg','index.html','Home','Hjem']];
     var directories = ['admission','about'];
-    /*function changeUrl(urlLink,placeNumb){
+    function changeUrl(urlLink,placeNumb){
         var a=urlLink[0].split('/');
         console.log(a[0]);
         console.log(a[0]==y);
@@ -36,7 +37,7 @@ function loadNavBar(){
     if (y==='about' || y==='admission'){
         menuElement[5] = ['../img/logo.svg','../index.html','Home','Hjem']
     }
-    menuElement.slice(0,-1).forEach(changeUrl);*/
+    menuElement.slice(0,-1).forEach(changeUrl);
 
     var listMain = document.createElement('UL');
     var listElm = document.createElement('LI');
@@ -83,7 +84,64 @@ function loadNavBar(){
     var mainPart = document.getElementById('wrapper');
     var x = document.getElementsByTagName("BODY")[0];
     x.insertBefore(nav,mainPart);
-};
+};*/
+function loadNavBar(){
+    var nav = document.createElement('DIV');
+    nav.id = 'navbar';
+    var menuElement=[
+        ['admission/index.html','Admission','Opptak'],
+        ['about/index.html','About us','Om oss'],
+        ['about/contact.html','Contact us','Kontakt oss'],
+        ['about/blog.html','Blog','Aktuelt'],
+        ['about/calendar.html','Calendar','Kalender'],
+        ['img/logo.svg','index.html','Home','Hjem']];
+
+    var listMain = document.createElement('UL');
+    var listElm = document.createElement('LI');
+    var linkElm = document.createElement('A');
+    listElm.setAttribute('style','text-align:left;');
+    linkElm.setAttribute('href',menuElement[0][0]);
+    linkElm.innerHTML=menuElement[0][1];
+    listElm.appendChild(linkElm);
+    listMain.appendChild(listElm);
+
+    var listImg = document.createElement('LI');
+    var linkElm2 = document.createElement('A')
+    linkElm2.setAttribute('href',menuElement[5][1]);
+    var imgElm=document.createElement('IMG');
+    imgElm.setAttribute('src',menuElement[5][0]);
+    imgElm.setAttribute('alt','Home');
+    imgElm.setAttribute('id','logo');
+    linkElm2.appendChild(imgElm);
+    listImg.appendChild(linkElm2);
+    listMain.appendChild(listImg);
+
+    var listElm2 = document.createElement('LI');
+    listElm2.setAttribute('style','text-align:right;cursor:pointer;');
+    listElm2.setAttribute('onclick','toggleClick()');
+    listElm2.innerHTML='About';
+    listMain.appendChild(listElm2);
+    nav.appendChild(listMain);
+
+    var divElem = document.createElement('DIV');
+    var listElmDiv = document.createElement('UL');
+    listElmDiv.setAttribute('id','toggleButtNav');
+    listElmDiv.setAttribute('style','display:none;');
+    function listMaker(iterNameVar){
+        var subMen = document.createElement('LI');
+        var subMenLink = document.createElement('A');
+        subMenLink.setAttribute('href',iterNameVar[0]);
+        subMenLink.innerHTML=iterNameVar[1];
+        subMen.appendChild(subMenLink);
+        listElmDiv.appendChild(subMen);
+    };
+    menuElement.slice(1,menuElement.length-1).forEach(listMaker);
+    divElem.appendChild(listElmDiv);
+    nav.appendChild(divElem);
+    var mainPart = document.getElementById('wrapper');
+    var x = document.getElementsByTagName("BODY")[0];
+    x.insertBefore(nav,mainPart);
+}
 window.onload=function(){
     document.getElementById("wrapper").addEventListener("load", loadNavBar());
 }
