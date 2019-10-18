ea682035af16d19c1044639f7cb9e657ed9c1c69
annabakkebo <annabakk@stud.ntnu.no>
2019-10-18 11:35:10 +0200

    footer js

--- Website/script/footer.js
+++ Website/script/footer.js
@@ -1,3 +1,25 @@
 function loadFooter() {
+    var footer = document.createElement("div");
+    footer.id = 'footer';
+    var links = [
+        ['index.html', 'Home', 'Hjem'],
+        ['admission/index.html','Admission','Opptak'],
+        ['about/index.html','About us','Om oss'],
+        ['about/blog.html','Blog','Aktuelt'],
+        ['about/calendar.html','Calendar','Kalender'],
+        ['mailto:post@laescuela.no','post@laescuela']
+    ];
+    var linkElm = document.createElement('a');
+    var listMain = document.createElement('ul');
+    var listElm = document.createElement('li');
+    listElm.setAttribute('style','text-align:left;');
+    linkElm.setAttribute('href',links[0][0]);
+    linkElm.innerHTML=links[0][1];
+    listElm.appendChild(linkElm);
+    listMain.appendChild(listElm);
 
+    footer.appendChild(listMain);
+    var doc = document.getElementsByTagName('body')[0];
+    console.log(doc);
+    doc.insertAdjacentElement("afterend", footer)
 }
