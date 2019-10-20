b48570557518dc1fbce5e9469da2eaa20f9b7224
annabakkebo <annabakk@stud.ntnu.no>
2019-10-20 13:41:56 +0200

    added elements to list in footer

--- Website/script/footer.js
+++ Website/script/footer.js
@@ -6,20 +6,45 @@ function loadFooter() {
         ['admission/index.html','Admission','Opptak'],
         ['about/index.html','About us','Om oss'],
         ['about/blog.html','Blog','Aktuelt'],
-        ['about/calendar.html','Calendar','Kalender'],
-        ['mailto:post@laescuela.no','post@laescuela']
+        ['about/calendar.html','Calendar','Kalender']
     ];
-    var linkElm = document.createElement('a');
-    var listMain = document.createElement('ul');
-    var listElm = document.createElement('li');
-    listElm.setAttribute('style','text-align:left;');
-    linkElm.setAttribute('href',links[0][0]);
-    linkElm.innerHTML=links[0][1];
-    listElm.appendChild(linkElm);
-    listMain.appendChild(listElm);
+    var linkListMain = document.createElement('ul');
+    for(const element of links){
+        console.log('going through the list');
+        var linkElm =document.createElement('a');
+        var listElm = document.createElement('li');
+        linkElm.setAttribute('style','text-align:left;');
+        linkElm.setAttribute('href',element[0]);
+        linkElm.innerHTML=element[1];
+        listElm.appendChild(linkElm);
+        linkListMain.appendChild(listElm);
+        console.log(linkElm);
+        console.log(linkListMain);
+    }
+    footer.appendChild(linkListMain);
+    console.log(footer);
 
-    footer.appendChild(listMain);
-    var doc = document.getElementsByTagName('body')[0];
-    console.log(doc);
-    doc.insertAdjacentElement("afterend", footer)
+
+    var adress = ['Vei veiesen 123','123 Sted','0047 123 45 678'];
+    var listAdress = document.createElement('ul');
+    listAdress.id= 'adress';
+    for (const element of adress){
+        var listElm = document.createElement('li');
+        listElm.innerHTML = element;
+        listAdress.appendChild(listElm);
+    }
+    var listElm =document.createElement('ul');
+    var linkElm = document.createElement('a');
+    linkElm.setAttribute('href', 'mailto:post@laescuela.no');
+    linkElm.innerHTML= 'post@laescuela';
+    listElm.appendChild(linkElm);
+    listAdress.appendChild(listElm);
+
+    console.log(listAdress);
+    footer.appendChild(listAdress);
+
+    console.log(footer);
+
+    var doc = document.getElementById('wrapper');
+    doc.insertAdjacentElement("afterend", footer);
 }
