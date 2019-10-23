866dd27816fa46025d20e404bb2472afc080d388
annabakkebo <annabakk@stud.ntnu.no>
2019-10-23 13:16:25 +0200

    Moved loadslideshow to slide.js
    Added the images to the slide.js file instead of in the html

--- Website/script/slide.js
+++ Website/script/slide.js
@@ -1,18 +1,53 @@
-function loadSlideshow(){
-  var i;
-  var root = document.querySelector('.fadein');
-  var els = root.querySelectorAll(':not(:first-child)');
+function loadSlideshow() {
+    var i;
+    var root = document.querySelector('.fadein');
+    var els = root.querySelectorAll(':not(:first-child)');
 
 
-  for (i=0; i < els.length; i++) {
-    els[i].classList.add('is-hidden');
-  }
-  root.addEventListener('transitionend', function(){
-    root.insertBefore(root.querySelector(':first-child.is-hidden'), null);
-  });
-  setInterval(function(){
-    root.querySelector(':first-child').classList.add('is-hidden');
-    root.querySelector(':nth-child(2)').classList.remove('is-hidden');
-  }, 3000)
+    for (i = 0; i < els.length; i++) {
+        els[i].classList.add('is-hidden');
+    }
+    root.addEventListener('transitionend', function () {
+        root.insertBefore(root.querySelector(':first-child.is-hidden'), null);
+    });
+    setInterval(function () {
+        root.querySelector(':first-child').classList.add('is-hidden');
+        root.querySelector(':nth-child(2)').classList.remove('is-hidden');
+    }, 3000)
 };
 
+
+/*
+<div id="slideshow" >
+  <div class="fadein" >
+      <img src="img/football.jpg">
+      <img src="img/library.jpg">
+      <img src="img/stroll.jpg">
+      <img src="img/study.jpg">
+      <img src="img/swing.jpg">
+      <img src="img/swing2.jpg">
+  </div>
+  <script> loadSlideshow()</script>
+</div>
+*/
+
+function loadSlideshow1() {
+    var imageList = ['img/football.jpg', 'img/library.jpg', 'img/stroll.jpg', 'img/study.jpg', 'img/swing.jpg', 'img/swing2.jpg'];
+    var slideshow = document.getElementById('slideshow');
+    var fadein = document.createElement("div");
+    fadein.className= 'fadein';
+    
+    for(const element of imageList){
+        var image= document.createElement('img');
+        image.setAttribute('src',element);
+        fadein.appendChild(image);
+    }
+    slideshow.insertAdjacentElement("afterend", fadein);
+    fadein.addEventListener('transitionend',function () {
+        fadein.insertBefore(fadein.querySelector(':first-child.is-hidden'),null);
+    });
+    setInterval(function () {
+        fadein.querySelector(':first-child').classList.add('is-hidden');
+        fadein.querySelector(':nth-child(2)').classList.remove('is-hidden');
+    },3000)
+}
