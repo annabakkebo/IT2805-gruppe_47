38c306a4181870be24c4b58a196c133d4288150e
annabakkebo <annabakk@stud.ntnu.no>
2019-10-24 15:43:28 +0200

    Loading the different employees

--- Website/script/contact.js
+++ Website/script/contact.js
@@ -1,24 +1,36 @@
+function loadContact() {
+    var wrapper = document.getElementById('wrapper');
+    var employeeDiv = document.createElement('div');
+    employeeDiv.id= 'employees';
+    wrapper.appendChild(employeeDiv);
+    loadEmployees();
+}
 
 function loadEmployees(employeeList=employeeList1) {
-    var wrapper = document.getElementById('wrapper');
-    var listOfEmployees = document.createElement('ul');
-    for (var person of employeeList){
+    var employeeDiv = document.getElementById('employees');
+    var listOfEmployees1 = document.createElement('ul');
+    listOfEmployees1.classList='employeeList';
+    listOfEmployees1.id='column1';
+    var listOfEmployees2 = document.createElement('ul');
+    listOfEmployees2.classList='employeeList';
+    listOfEmployees2.id='column2';
+    for (var i=0; i< employeeList.length;i++){
         console.log('Going through the employee list');
         var employeelistElm = document.createElement('li');
-        employeelistElm.class = "employeeListElm";
+        employeelistElm.classList = "employeeListElm";
         var personList = document.createElement('ul');
 
-        var name = person.fullName();
+        var name = employeeList[i].fullName();
         var nameListElm = document.createElement('li');
         nameListElm.innerHTML=name;
         personList.appendChild(nameListElm);
 
-        var workTitle = person.workTitle;
+        var workTitle = employeeList[i].workTitle;
         var workListElm = document.createElement('li');
         workListElm.innerHTML=workTitle;
         personList.appendChild(workListElm);
 
-        var email = person.email();
+        var email = employeeList[i].email();
         var emailListElm = document.createElement('li');
         var emailLinkElm = document.createElement('a');
         emailLinkElm.setAttribute('href', 'mailto:'+email);
@@ -26,14 +38,23 @@ function loadEmployees(employeeList=employeeList1) {
         emailListElm.appendChild(emailLinkElm);
         personList.appendChild(emailListElm);
 
-        var tlf = person.tlf;
+        var tlf = employeeList[i].tlf;
         var tlfListElm = document.createElement('li');
         tlfListElm.innerHTML="Tlf: "+tlf;
         personList.appendChild(tlfListElm);
 
         employeelistElm.appendChild(personList);
-        listOfEmployees.appendChild(employeelistElm);
+        if (i%2==0){
+            listOfEmployees1.appendChild(employeelistElm);
+        }
+        else{
+            listOfEmployees2.appendChild(employeelistElm);
+        }
     };
-    wrapper.appendChild(listOfEmployees);
+    while (employeeDiv.hasChildNodes()){
+        employeeDiv.removeChild(employeeDiv.childNodes[0]);
+    }
+    employeeDiv.appendChild(listOfEmployees1);
+    employeeDiv.appendChild(listOfEmployees2);
 
 }
