0034f064cf3de0c7603a7588acdb3f365948559e
annabakkebo <annabakk@stud.ntnu.no>
2019-10-24 16:09:12 +0200

    Sorting contacts from lastName and adding more people

--- Website/script/contactInfo.js
+++ Website/script/contactInfo.js
@@ -1,93 +1,201 @@
-var Person1 ={
+var Person1 = {
     firstName: "Arne",
     lastName: "Brillestad",
-    id : 1,
+    category: 'leadership',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Principal",
     email: function () {
-        return this.firstName+"."+this.lastName+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "91662277"
 };
-var Person2 ={
+var Person2 = {
     firstName: "Hanne",
     lastName: "Johannesen",
-    id : 2,
+    category: 'teacher',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Teacher",
     email: function () {
-        return this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "91110932"
 };
-var Person3 ={
+var Person3 = {
     firstName: "Joy",
     lastName: "McAuley",
-    id : 3,
+    category: 'teacher',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Teacher",
     email: function () {
-        return this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "45826144"
 };
-var Person4 ={
+var Person4 = {
     firstName: "Martin",
     lastName: "Hansen",
-    id : 4,
+    category: 'teacher',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Teacher",
     email: function () {
-        return this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "91916377"
 };
-var Person5 ={
+var Person5 = {
     firstName: "Eve",
     lastName: "Johnson",
-    id : 5,
+    category: 'teacher',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Teacher",
     email: function () {
-        return this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "95824188"
 };
-var Person6 ={
+var Person6 = {
     firstName: "Markus",
     lastName: "Torsen",
-    id : 6,
+    category: 'teacher',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Teacher",
     email: function () {
-        return this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "93821568"
 };
-var Person7 ={
+var Person7 = {
     firstName: "André",
     lastName: "Lunde",
-    id : 7,
+    category: 'teacher',
     fullName: function () {
-        return this.firstName + " "+ this.lastName;
+        return this.firstName + " " + this.lastName;
     },
     workTitle: "Teacher",
     email: function () {
-        return this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@laeskuela.no"
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
     },
-    tlf: "12345678"
+    tlf: "72916382"
+};
+var Person8 = {
+    firstName: 'Trude',
+    lastName: 'Thorsen',
+    category: 'leadership',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Administator",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "91627382"
+};
+var Person9 = {
+    firstName: 'Sigurd',
+    lastName: 'Magnusson',
+    category: 'leadership',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Administator",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "91627382"
+};
+var Person10 = {
+    firstName: 'Name',
+    lastName: 'Nameson',
+    category: 'counsellor',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Counsellor",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "91110245"
+};
+var Person11 = {
+    firstName: 'Name',
+    lastName: 'Nameson',
+    category: 'counsellor',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Counsellor",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "15241099"
+};
+var Person12 = {
+    firstName: 'Gjermund',
+    lastName: 'Nilsen',
+    category: 'external',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Rented assistant",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "71524311"
+};
+var Person13 = {
+    firstName: 'Jakob',
+    lastName: 'Andersen',
+    category: 'external',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Substitute teacher",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "71524311"
+};
+var Person14 = {
+    firstName: 'Gjermund',
+    lastName: 'Nilsen',
+    category: 'external',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Consultant",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "71524311"
+};
+var Person15 = {
+    firstName: 'Finn',
+    lastName: 'Asbjørnsen',
+    category: 'external',
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+    workTitle: "Teacher assistant, rented",
+    email: function () {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@laeskuela.no"
+    },
+    tlf: "81627155"
 };
 
-var employeeList1 = [Person1,Person2,Person3,Person4,Person5,Person6,Person7];
+var employeeList1 = [
+    Person1, Person2, Person3, Person4, Person5, Person6, Person7, Person8, Person9,
+    Person10, Person11, Person12, Person13, Person14, Person15
+];
+employeeList1.sort((a,b)=>(a.lastName>b.lastName)?1:-1);
